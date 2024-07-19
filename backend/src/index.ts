a26af074e0505da981d421def97830cf0b35f104
VishruthVS <88739731+VishruthVS@users.noreply.github.com>
2024-07-20 05:12:12 +0530

    frontend

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -7,23 +7,29 @@ let receiverSocket: null | WebSocket = null;
 
 wss.on('connection', function connection(ws) {
     ws.on('error', console.error);
+
     ws.on('message', function message(data: any) {
         const message = JSON.parse(data);
         if (message.type === 'sender') {
+            console.log("sender added");
             senderSocket = ws;
         } else if (message.type === 'receiver') {
+            console.log("receiver added");
             receiverSocket = ws;
         } else if (message.type === 'createOffer') {
             if (ws !== senderSocket) {
                 return;
             }
+            console.log("sending offer");
             receiverSocket?.send(JSON.stringify({ type: 'createOffer', sdp: message.sdp }));
         } else if (message.type === 'createAnswer') {
             if (ws !== receiverSocket) {
                 return;
             }
+            console.log("sending answer");
             senderSocket?.send(JSON.stringify({ type: 'createAnswer', sdp: message.sdp }));
         } else if (message.type === 'iceCandidate') {
+            console.log("sending ice candidate")
             if (ws === senderSocket) {
                 receiverSocket?.send(JSON.stringify({ type: 'iceCandidate', candidate: message.candidate }));
             } else if (ws === receiverSocket) {
@@ -31,4 +37,5 @@ wss.on('connection', function connection(ws) {
             }
         }
     });
+
 });
